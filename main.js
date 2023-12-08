a93b1529d35205a2807a61d47b605ccd21e8b71e
soumi1898 <153014671+soumi1898@users.noreply.github.com>
2023-12-08 22:39:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var form=document.getElementById('addForm');
-var itemsList=document.getElementById('items');
-var filter=document.getElementById('filter');
+// var itemsList=document.getElementById('items');
+// var filter=document.getElementById('filter');
 //Form submit event
 form.addEventListener('submit',addItem);
 //delete event
-itemsList.addEventListener('click',removeItem);
-//Filter event
-filter.addEventListener('keyup',filterItems);
+// itemsList.addEventListener('click',removeItem);
+// //Filter event
+// filter.addEventListener('keyup',filterItems);
 
 //add item
 function addItem(e)
@@ -14,69 +14,37 @@ function addItem(e)
     e.preventDefault();
 
 //get input
-var newItem=document.getElementById('item').value;
+localStorage.setItem(document.getElementById('username').value);
+localStorage.setItem(document.getElementById('email').value);
 
+//console.log(localStorage.setItem(document.getElementById('username').value));
+//console.log(localStorage.setItem(document.getElementById('email').value));
+
+}
 //create new li element
-var li=document.createElement('li');
+// var li=document.createElement('li');
 
-//add class
-li.className='list-group-item';
+// //add class
+// li.className='list-group-item';
 
-//add text node
-li.appendChild(document.createTextNode(newItem));
+// //add text node
+// li.appendChild(document.createTextNode(newItem));
 
-//create delete button
-var deleteBtn=document.createElement('button');
+// //create delete button
+// var deleteBtn=document.createElement('button');
 
-//add class name for button
-deleteBtn.className="btn btn-danger btn-sm float-right delete";
+// //add class name for button
+// deleteBtn.className="btn btn-danger btn-sm float-right delete";
 
-//append text node to button
-deleteBtn.appendChild(document.createTextNode('X'));
+// //append text node to button
+// deleteBtn.appendChild(document.createTextNode('X'));
 
-//append button to li
-li.appendChild(deleteBtn);
+// //append button to li
+// li.appendChild(deleteBtn);
 
-//append li to list
-itemsList.appendChild(li);
-
-
-//console.log(li);
-}
-
-//remove Item
-
-function removeItem(e)
-{
-
-    if(e.target.classList.contains('delete'))
-    {
-        if(confirm('Are you sure?'))
-        {
-            var li=e.target.parentElement;
-            itemsList.removeChild(li);
-        }
-
-    }
-}
+// //append li to list
+// itemsList.appendChild(li);
 
 
-//Filter items
-function filterItems(e)
-{
-    var text=e.target.value.toLowerCase();
-    var items=itemsList.getElementsByTagName('li');
-   // console.log(text);
-   Array.from(items).forEach(function(item){
-        var itemName= item.firstChild.textContent;
-        //console.log(itemName);
-        if(itemName.toLowerCase().indexOf(text)!=-1)
-        {
-            item.style.display='block';
-        }
-        else
-        {
-            item.style.display='none';
-        }
-   });
-}
+// //console.log(li);
+// }
